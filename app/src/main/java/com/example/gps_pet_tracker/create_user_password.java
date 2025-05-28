5bfb057b7335117055b533f6d55b9931b757fa57
PoohZaah <maklord.angeles88@gmail.com>
2025-05-29 06:49:49 +0800

    debbigged

--- app/src/main/java/com/example/gps_pet_tracker/create_user_password.java
+++ app/src/main/java/com/example/gps_pet_tracker/create_user_password.java
@@ -8,7 +8,7 @@ public class create_user_password extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user_password);
     }
 }
